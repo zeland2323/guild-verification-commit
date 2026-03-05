69ea984717985960d1ad5db65a288148597cee2e
Marco Ferreira <98695331+zeland2323@users.noreply.github.com>
2026-03-05 22:33:52 +0000

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:33:47.539Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T22:33:51.851Z
+Commit: Fix CSS styling bug on mobile devices
