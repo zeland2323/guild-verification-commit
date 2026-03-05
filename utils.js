c9d86b4324237d59dd14dd65308419bb7fd8725a
Marco Ferreira <98695331+zeland2323@users.noreply.github.com>
2026-03-05 22:40:02 +0000

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:39:45.262Z
-Commit: Add dark mode support
+Timestamp: 2026-03-05T22:40:01.440Z
+Commit: Resolve null pointer exception in user service
