0727c90a923025d2c77fc9ce02589d9ef7596d62
Marco Ferreira <98695331+zeland2323@users.noreply.github.com>
2026-03-05 22:34:33 +0000

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:34:28.275Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T22:34:33.162Z
+Commit: Resolve null pointer exception in user service
