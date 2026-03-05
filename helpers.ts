e1e281dcd297a1b2c831b69636bf9a5a205cdad0
Marco Ferreira <98695331+zeland2323@users.noreply.github.com>
2026-03-05 22:34:48 +0000

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:34:36.292Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T22:34:47.821Z
+Commit: Fix CSS styling bug on mobile devices
