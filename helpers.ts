f4e44dfe57d2844e8ed381f10c2bb7d2451cf4fc
Marco Ferreira <98695331+zeland2323@users.noreply.github.com>
2026-03-05 22:34:28 +0000

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:33:53.075Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T22:34:28.275Z
+Commit: Introduce real-time notifications
