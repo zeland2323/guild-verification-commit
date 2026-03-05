06ded8d6f090c212e182f4972cea4fb522431752
Marco Ferreira <98695331+zeland2323@users.noreply.github.com>
2026-03-05 22:34:17 +0000

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:34:03.194Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T22:34:16.411Z
+Commit: Fix CSS styling bug on mobile devices
