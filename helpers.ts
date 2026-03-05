3ef434ef5e12e1704448635a3225fd86fd70446d
Marco Ferreira <98695331+zeland2323@users.noreply.github.com>
2026-03-05 22:32:52 +0000

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:32:50.505Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T22:32:51.609Z
+Commit: Patch API endpoint error handling
